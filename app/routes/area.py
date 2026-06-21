ab412e8943b2228e913aefefdcb76099ce6441ec
Md.Sami Ul Haque <samiulh4@gmail.com>
2026-06-22 00:30:33 +0600

    store

--- app/routes/area.py
+++ app/routes/area.py
@@ -198,3 +198,84 @@ async def get_districts_json():
         
     return {"data_not_found": data_not_found}
 
+@router.get('/area/divisions/store')
+async def area_divisions_store():
+    data_dir = Path(__file__).parents[2] / "data"
+    with open(data_dir / "divisions.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    divisions = data['divisions']
+    for division in divisions:
+        await areas_collection.insert_one({
+            "area_parent_id": None,
+            "area_type": "division",
+            "area_name_en": division['name'].strip(),
+            "area_name_local": division['bn_name'].strip(),
+            "area_latitude": division['lat'].strip(),
+            "area_longitude": division['long'].strip(),
+            "division_id": int(division['id'].strip())
+        })
+    return { "message": f"Successfully inserted {len(divisions)} divisions" }
+# end of router area_divisions_store()
+
+@router.get('/area/districts/store')
+async def area_districts_store():
+    data_dir = Path(__file__).parents[2] / "data"
+    with open(data_dir / "districts.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    districts = data['districts']
+    for district in districts:
+        await areas_collection.insert_one({
+            "area_parent_id": None,
+            "area_type": "district",
+            "area_name_en": district['name'].strip(),
+            "area_name_local": district['bn_name'].strip(),
+            "area_latitude": district['lat'].strip(),
+            "area_longitude": district['long'].strip(),
+            "division_id": int(district['division_id'].strip()),
+            "district_id": int(district['id'].strip())
+        })
+    return { "message": f"Successfully inserted {len(districts)} districts" }
+# end of router area_districts_store()
+
+@router.get('/area/upazilas/store')
+async def area_upazilas_store():
+    data_dir = Path(__file__).parents[2] / "data"
+    with open(data_dir / "upazilas.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    upazilas = data['upazilas']
+    for upazila in upazilas:
+        await areas_collection.insert_one({
+            "area_parent_id": None,
+            "area_type": "upazila",
+            "area_name_en": upazila['name'].strip(),
+            "area_name_local": upazila['bn_name'].strip(),
+            "area_latitude": None,
+            "area_longitude": None,
+            "district_id": int(upazila['district_id'].strip()),
+            "upazila_id": int(upazila['id'].strip()),
+        })
+    return { "message": f"Successfully inserted {len(upazilas)} upazilas" }
+# end of router area_upazilas_store()
+
+@router.get('/area/cities/store')
+async def area_cities_store():
+    data_dir = Path(__file__).parents[2] / "data"
+    with open(data_dir / "cities.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    cities = data['dhaka']
+    for city in cities:
+        await areas_collection.insert_one({
+            "area_parent_id": None,
+            "area_type": "city",
+            "area_name_en": city['name'].strip(),
+            "area_name_local": city['bn_name'].strip(),
+            "area_latitude": None,
+            "area_longitude": None,
+            "division_id": int(city['division_id'].strip()),
+            "district_id": int(city['district_id'].strip()),
+            "meta_data": {
+                "city_corporation": city['city_corporation'].strip()
+            }
+        })
+    return { "message": f"Successfully inserted {len(cities)} cities" }
+# end of router area_cities_store()
